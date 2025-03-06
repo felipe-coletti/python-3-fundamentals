ac98b8e3ed22a114b523305a009a09b5d30127a1
felipe-coletti <felipecoletti2@gmail.com>
2025-03-06 08:20:58 -0300

    refactor(module-002/exercise-019): simplifies results storage

--- module-002/exercise-019.py
+++ module-002/exercise-019.py
@@ -6,17 +6,15 @@ from datetime import date
 
 currentYear = date.today().year
 
-groups = {
-    'major': 0,
-    'minor': 0
-}
+major = 0
+minor = 0
 
 for i in range(0, 7):
     birthYear = int(input('Digite o ano de nascimento da {}ª pessoa: '.format(i + 1)))
     
     if currentYear - birthYear >= 18:
-        groups['major'] += 1
+        major += 1
     else:
-        groups['minor'] += 1
+        minor += 1
         
-print('{} pessoas são maiores de idade e {} são menores de idade.'.format(groups['major'], groups['minor']))
+print('{} pessoas são maiores de idade e {} são menores de idade.'.format(major, minor))
